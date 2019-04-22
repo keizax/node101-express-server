29d49d22cf4a39a45dc1edfade98d240ff430679
JGadi <johnpgadi@gmail.com>
2019-04-22 12:59:58 -0700

    first express server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
-
+var data = require("./data.json");
 
 // create your express server below
-var app;
+var express = require('express')
+var morgan = require('morgan');
+var app = express();
 
 // add your routes and middleware below
-
+app.use(morgan('dev'));
+app.get('/', function(req, res) {
+    res.status(200)
+    res.send('first express server');
+});
+app.get('/', (req, res) => {
+        res.send('Hello Express');
+    });
+app.get("/data", function(req, res) {
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify(data));        
+});
 // finally export the express application
 module.exports = app;
